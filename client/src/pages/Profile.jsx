0da3fcc9e32d2d0f201f8841a835617879602611
Leonard <Leonard009w@gmail.com>
2025-09-02 10:06:52 +0200

    updated profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,22 @@
 
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
 
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user)
+
+  //firebase storage
+       // allow read; 
+     // allow write: if request.resource.size < 2 * 1024 * 1024 &&
+     // request.resource.contentType.matches('image/.*')
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" 
+        <input type="file" ref={fileRef} hidden accept='image/*'/>
+        <img onClick={() => fileRef.current.click()} src={currentUser.avatar} alt="profile" 
         className='rounded-full h-20 w-20 object-cover cursor-pointer self-center mt-2' />
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
